aa82d519de1774803ae838b2b2c98045c74a7575
Xinyu Luan <xiluan@ucsd.edu>
2021-05-06 16:46:49 -0700

    Added Dog Microservice

---
+++ DogMicroservice/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO dog (id, name, breed, origin) VALUES (1, 'book','A popular book', 'A');
+INSERT INTO dog (id, name, breed, origin) VALUES (2, 'candy','A favorite candy', 'B');
+INSERT INTO dog (id, name, breed, origin) VALUES (3, 'toy','A simple toy', 'C');
+INSERT INTO dog (id, name, breed, origin) VALUES (4, 'food','Your favorite food item', 'D');
+INSERT INTO dog (id, name, breed, origin) VALUES (5, 'tv','A flat screen TV', 'E');
